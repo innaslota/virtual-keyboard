9dd2cdff5bfa76e259fba71aa70788d64e04a5f5
innaslota <innaslota8@gmail.com>
2023-04-30 12:32:03 +0300

    feat: add basic styles

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ keyboard.classList.add('keyboard');
 
 
 const keys = [
-  '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace',
-  'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\',
+  '~', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace',
+  'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\',
   'CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'Enter',
-  'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'Shift',
-  'Ctrl', 'Alt', 'Space', 'Alt', 'Ctrl'
+  'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'Shift', '↑',
+  'Ctrl', 'Win', 'Alt', 'Space', 'Alt', 'Ctrl', '←', '↓', '→'
 ];
 
 keys.forEach(key => {
