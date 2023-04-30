6413ad7d532dfc7461598af1e111580f9deec3b2
innaslota <innaslota8@gmail.com>
2023-04-30 15:57:51 +0300

    feat: add input and style it; start adding a feature for highlighting a button on press (working partially)

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const keyboard = document.createElement('div');
 keyboard.classList.add('keyboard');
 
+const input = document.createElement('input');
+input.classList.add('text-input');
 
 const keys = [
   '~', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace',
@@ -14,7 +16,37 @@ keys.forEach(key => {
   const button = document.createElement('button');
   button.classList.add('key');
   button.textContent = key;
+  console.log(key);
+  button.setAttribute('data-code', key);
   keyboard.appendChild(button);
 });
 
+document.body.appendChild(input);
 document.body.appendChild(keyboard);
+
+const keyState = {};
+
+document.addEventListener('keydown', event => {
+  const keyCode = event.code;
+  console.log(keyCode);
+  if (keyState[keyCode]) {
+    return;
+  }
+  
+  const button = document.querySelector(`.key[data-code="${keyCode}"]`);
+  if (button) {
+    button.classList.add('active');
+  }
+  keyState[keyCode] = true;
+});
+
+document.addEventListener('keyup', event => {
+  const keyCode = event.code;
+  const button = document.querySelector(`.key[data-code="${keyCode}"]`);
+  if (button) {
+    button.classList.remove('active');
+  }
+  keyState[keyCode] = false;
+});
+
+
